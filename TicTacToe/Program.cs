c584c61707d8994ddeb9819764e4c08b44a895ec
TiaanVW <vanwyktiaan@gmail.com>
2022-04-06 11:28:38 +0200

    Winning functionality added

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -17,6 +17,7 @@ namespace TicTacToe
                 {"7","8","9"}
             };
             bool inputCorrect = true;
+            bool gameComplete = false;
 
             WriteGrid(playField);
 
@@ -35,7 +36,7 @@ namespace TicTacToe
                 {
                     Console.Write($"Player {player}: Please choose your field! ");
                     input = Console.ReadLine();
-                } while (!inputCorrect);
+                } while (!inputCorrect && (gameComplete != true));
 
                 switch (player)
                 {
@@ -50,22 +51,25 @@ namespace TicTacToe
                         WriteGrid(playField);
                         break;
                 }
-            } while (true);
 
-            
+                gameComplete = WinCheck(playField, player);
+
+            } while (gameComplete == false);
+
+
             Console.Read();
         }
         public static void WriteGrid(string[,] numbers)
         {
-            
+
             Console.WriteLine("     |     |     ");
-            Console.WriteLine($"  {numbers[0,0]}  |  {numbers[0,1]}  |  {numbers[0,2]}  ");
+            Console.WriteLine($"  {numbers[0, 0]}  |  {numbers[0, 1]}  |  {numbers[0, 2]}  ");
             Console.WriteLine("_____|_____|_____");
             Console.WriteLine("     |     |     ");
-            Console.WriteLine($"  {numbers[1,0]}  |  {numbers[1,1]}  |  {numbers[1,2]}  ");
+            Console.WriteLine($"  {numbers[1, 0]}  |  {numbers[1, 1]}  |  {numbers[1, 2]}  ");
             Console.WriteLine("_____|_____|_____");
             Console.WriteLine("     |     |     ");
-            Console.WriteLine($"  {numbers[2,0]}  |  {numbers[2,1]}  |  {numbers[2,2]}  ");
+            Console.WriteLine($"  {numbers[2, 0]}  |  {numbers[2, 1]}  |  {numbers[2, 2]}  ");
             Console.WriteLine("     |     |     ");
         }
         public static void XorO(int player, string input, string[,] numbers)
@@ -107,6 +111,52 @@ namespace TicTacToe
                     numbers[2, 2] = playerSign;
                     break;
             }
+        }
+        public static bool WinCheck(string[,] playField, int playerNumber)
+        {
+            if ((playField[0, 0] == playField[1, 1]) && (playField[1, 1] == playField[2, 2]))
+            {
+                Console.WriteLine($"Player {playerNumber} won!");
+                return true;
+            }
+            else if ((playField[0, 0] == playField[0, 1]) && (playField[0, 1] == playField[0, 2]))
+            {
+                Console.WriteLine($"Player {playerNumber} won!");
+                return true;
+            }
+            else if ((playField[1, 0] == playField[1, 1]) && (playField[1, 1] == playField[1, 2]))
+            {
+                Console.WriteLine($"Player {playerNumber} won!");
+                return true;
+            }
+            else if ((playField[2, 0] == playField[2, 1]) && (playField[2, 1] == playField[2, 2]))
+            {
+                Console.WriteLine($"Player {playerNumber} won!");
+                return true;
+            }
+            else if ((playField[2, 0] == playField[1, 1]) && (playField[0, 2] == playField[1, 1]))
+            {
+                Console.WriteLine($"Player {playerNumber} won!");
+                return true;
+            }
+            else if ((playField[0, 0] == playField[1, 0]) && (playField[1, 0] == playField[2, 0]))
+            {
+                Console.WriteLine($"Player {playerNumber} won!");
+                return true;
+            }
+            else if ((playField[0, 1] == playField[1, 1]) && (playField[2, 1] == playField[1, 1]))
+            {
+                Console.WriteLine($"Player {playerNumber} won!");
+                return true;
+            }
+            else if ((playField[0, 2] == playField[1, 2]) && (playField[1, 2] == playField[2, 2]))
+            {
+                Console.WriteLine($"Player {playerNumber} won!");
+                return true;
+            }
+            else
+                Console.WriteLine();
+            return false;
         }
     }
 }
